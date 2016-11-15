4a26f9ab30a2532230da6a4f63d0336af044d1a8
Ryan <caixuguang@unity3d.com>
2016-11-15 19:31:59 +0800

    create branch

---
+++ handlers.article.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func showIndexPage(c *gin.Context) {
+	articles := getAllArticles()
+	render(c, gin.H{
+		"title":"home page",
+		"payload":articles,
+	}, "index.html")
+}
+
+func getArticle(c *gin.Context) {
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		if article, err := getArticleById(articleID); err == nil {
+			c.HTML(
+				http.StatusOK,
+				"article.html",
+				gin.H{
+					"title": article.Title,
+					"payload": article,
+				},
+			)
+		} else {
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
+
+}
